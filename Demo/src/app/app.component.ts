4e8b594380b29507c5eaf3a4ce1336c01a72b3fe
Tanvir <k.tanvir.hossain@gmail.com>
2020-04-01 18:26:12 +0600

    custom bindings

--- Demo/src/app/app.component.ts
+++ Demo/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Demo';
+  feature = 'recipes';
+  onNevigate(feature: string){
+    this.feature = feature;
+  }
 }
